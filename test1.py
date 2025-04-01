36d18fcc4c79b48b76d61de9dc94c4d4019f439d
Santhosh M <88887598+ansSanthoshM@users.noreply.github.com>
2025-04-01 15:47:42 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+import os
+import datetime
+import pytz
+
+ist = pytz.timezone('Asia/Kolkata')
+timestamp_ist = datetime.datetime.now(ist).strftime("%Y%m%d%H%M%S")
+print(f"Current Data & Time: {timestamp_ist}")
